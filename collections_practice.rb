6e3347c67f43b68d086d1696c4a25455447dbae3
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-12 22:16:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -130,7 +130,7 @@ def organize_schools(schools)
 
   schools.each do |school, location|
     location.each do |key, val|
-      binding.pry
+      #binding.pry
       if result[val] == nil
         result[val] = [school]
       else
